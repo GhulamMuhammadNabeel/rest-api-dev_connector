3aed75aa35ee72fcd567b195afe2bab057968f9f
Nabeel <apnaqureshi5555@gmail.com>
2025-03-26 16:42:02 +0500

    feat: remove unused files and components to clean up the project structure

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
  
 require('dotenv').config();
   
-const connectDB = async () => {
-        await mongoose.connect(process.env.MONGO_URI)
+const connectDB = async () => { 
+        await mongoose.connect(process.env.MONGO_URI, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            })
         .then(()=>{console.log('MongoDB connected...');})
         .catch((err)=>console.log(err))
 };
